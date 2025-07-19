0bf9f04bd62583c609b8701f25a88a837bdc4c2c
anirudh <agrwlanirudh@gmail.com>
2025-07-19 14:10:20 +0800

    Enabled google test

--- test/test_trade_processor.cpp
+++ test/test_trade_processor.cpp
@@ -1,3 +1,12 @@
 //
 // Created by Anirudh Agrawal on 7/19/25.
 //
+
+#include "TradeProcessor.h"
+
+#include "gtest/gtest.h"
+
+TEST(TradeProcessor, SingleTradeFIFO) {
+    TradeProcessor tp("fifo");
+
+}
