cd101a6c2fc646b0bf6ef755fcc7a16bcaa3dc56
anirudh <agrwlanirudh@gmail.com>
2025-07-19 14:45:10 +0800

    Add the missing handling for 2 digit precision in the output.

--- src/PnlPublisher.cpp
+++ src/PnlPublisher.cpp
@@ -5,9 +5,11 @@
 #include "PnlPublisher.h"
 
 #include <iostream>
+#include <iomanip>
 
 PnlPublisher::~PnlPublisher() noexcept = default;
 
 void PnlPublisher::Publish(uint32_t timestamp, std::string_view symbol, double pnl) noexcept {
-    std::cout << timestamp << "," << symbol << "," << pnl << std::endl;
+    std::cout << timestamp << "," << symbol << ",";
+    std::cout << std::fixed << std::setprecision(2) << pnl << "\n";
 }
